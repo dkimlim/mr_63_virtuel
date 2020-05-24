03fc2f4437e234ceca90fb8afa26aa2a2958a916
diane1234 <dianekimlim@gmail.com>
2020-05-24 15:35:05 -0400

    remove header

--- src/App.js
+++ src/App.js
@@ -4,10 +4,6 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        MR 63 HEADER
-      </header>
-
       <div className="Home">
         <iframe width="100%" height="800" scrolling="no" src=" https://momento360.com/e/u/5d047cf1066a4b08ab73980fa9f96dad?utm_campaign=embed&utm_source=other&heading=0&pitch=0&field-of-view=75"></iframe>
       </div>
