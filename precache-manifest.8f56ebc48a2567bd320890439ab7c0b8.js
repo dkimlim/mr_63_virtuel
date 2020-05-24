4770665b6071b53e85961eb5f0145a55fffb5fcd
diane1234 <dianekimlim@gmail.com>
2020-05-24 15:19:20 -0400

    Updates

---
+++ precache-manifest.8f56ebc48a2567bd320890439ab7c0b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5ec4b1262d338714d1956fc1785a1ae",
+    "url": "//dkimlim.github.io/mr_63_virtuel/index.html"
+  },
+  {
+    "revision": "8c5de503176a83042411",
+    "url": "//dkimlim.github.io/mr_63_virtuel/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7801bfc80a3313b373ea",
+    "url": "//dkimlim.github.io/mr_63_virtuel/static/js/2.2407abc4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "//dkimlim.github.io/mr_63_virtuel/static/js/2.2407abc4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c5de503176a83042411",
+    "url": "//dkimlim.github.io/mr_63_virtuel/static/js/main.044c062a.chunk.js"
+  },
+  {
+    "revision": "1233e6fa52b1774225b7",
+    "url": "//dkimlim.github.io/mr_63_virtuel/static/js/runtime-main.5e141dc2.js"
+  }
+]);
